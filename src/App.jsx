c7d3daba131d24080228f07bf1c2c03dcc894277
Clara Genovese <clariigenovese@gmail.com>
2022-08-24 14:18:01 -0300

    feat(App): add footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,20 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import './App.css'
+import Footer from './components/Footer'
 import Nav from './components/Nav'
 import RouteOne from './pages/RouteOne'
 import RouteTwo from './pages/RouteTwo'
 
 function App() {
   return (
-    <div className="">
+    <div>
       <Nav />
       <Routes >
         <Route path='/' element={<RouteOne />} />
         <Route path='/routeTwo' element={<RouteTwo />} />
       </Routes>
+      <Footer />
     </div>
   )
 }
